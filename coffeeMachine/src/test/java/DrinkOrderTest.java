30c4e287853ca9eb442497406a0608914e3de451
sabinef <sabine.ferreira@lcdlv.fr>
2017-10-27 01:17:08 +0200

    test stick

--- coffeeMachine/src/test/java/DrinkOrderTest.java
+++ coffeeMachine/src/test/java/DrinkOrderTest.java
@@ -9,6 +9,7 @@ public class DrinkOrderTest {
     private static final String DRINK_MAKER_MAKES_1_CHOCOLATE = "Drink maker makes 1 chocolate with no sugar(s) and therefore no stick";
     private static final String DRINK_MAKER_MAKES_1_COFFEE_WITH_1_SUGAR = "Drink maker makes 1 coffee with 1 sugar(s) and a stick";
     private static final String DRINK_MAKER_MAKES_1_COFFEE_WITH_2_SUGAR = "Drink maker makes 1 coffee with 2 sugar(s) and a stick";
+    private static final Object DRINK_MAKER_MAKES_1_COFFEE_WITH_2_SUGAR_AND_A_STICK = "Drink maker makes 1 coffee with 2 sugar(s) and a stick";
 
     @Test
     public void should_send_one_coffee_order_when_customer_order_coffe() {
@@ -40,5 +41,12 @@ public class DrinkOrderTest {
         DrinkOrder drinkOrder = new DrinkOrder(DrinkType.COFFEE);
         drinkOrder.addSugarAndStick(2);
         assertThat(drinkOrder.createMessageToDrinkMaker()).isEqualTo(DRINK_MAKER_MAKES_1_COFFEE_WITH_2_SUGAR);
+    }
+
+    @Test
+    public void should_send_one_coffee_with_two_sugar_and_one_stick_order_to_coffee_maker() {
+        DrinkOrder drinkOrder = new DrinkOrder(DrinkType.COFFEE);
+        drinkOrder.addSugarAndStick(2);
+        assertThat(drinkOrder.createMessageToDrinkMaker()).isEqualTo(DRINK_MAKER_MAKES_1_COFFEE_WITH_2_SUGAR_AND_A_STICK);
     }
 }
