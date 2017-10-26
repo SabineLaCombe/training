57657e421d3338e0b32ea30876ff336dc3edb707
sabinef <sabine.ferreira@lcdlv.fr>
2017-10-27 01:13:07 +0200

    refacto methode

--- coffeeMachine/src/main/java/DrinkOrder.java
+++ coffeeMachine/src/main/java/DrinkOrder.java
@@ -1,34 +1,28 @@
+import java.text.MessageFormat;
+
 public class DrinkOrder {
 
     private DrinkType drinkType;
-    private int numberOfSugar;
+    private String numberOfSugar;
+    private String stick;
 
-    private static final String MESSAGE_TO_DRINK_MAKER_PATTERN = "Drink maker makes 1 ";
-    private static final String ADD_ONE_SUGAR_AND_A_STICK_TO_TEMPLATE_MESSAGE = " with 1 sugar and a stick";
-    private static final String ADD_TWO_SUGARS_AND_A_STICK_TO_MESSAGE_TEMPLATE = " with 2 sugars and a stick";
-    private static final String ADD_NO_SUGAR_AND_NO_STICK = " with no sugar and therefore no stick";
+    private static final String ORDER_MESSAGE_PATTERN = "Drink maker makes 1 {0} with {1} sugar(s) and {2} stick";
+    private static final String NO_SUGAR = "no";
+    private static final String NO_STICK = "therefore no";
+    private static final String ONE_STICK = "a";
 
     DrinkOrder(DrinkType drinkType) {
         this.drinkType = drinkType;
+        this.numberOfSugar = NO_SUGAR;
+        this.stick = NO_STICK;
     }
 
     String createMessageToDrinkMaker() {
-        StringBuilder message = new StringBuilder(MESSAGE_TO_DRINK_MAKER_PATTERN + drinkType.getDrinkLabel());
-        message = addSugarAndStickOrderToMessage(message);
-        return message.toString();
+        return MessageFormat.format(ORDER_MESSAGE_PATTERN, drinkType.getDrinkLabel(), numberOfSugar, stick);
     }
 
-    private StringBuilder addSugarAndStickOrderToMessage(StringBuilder message) {
-        if(numberOfSugar == 1) {
-            return message.append(ADD_ONE_SUGAR_AND_A_STICK_TO_TEMPLATE_MESSAGE);
-        } else if (numberOfSugar == 2) {
-            return message.append(ADD_TWO_SUGARS_AND_A_STICK_TO_MESSAGE_TEMPLATE);
-        } else {
-            return message.append(ADD_NO_SUGAR_AND_NO_STICK);
-        }
-    }
-
-    void addSugar(int quantity) {
-        numberOfSugar = quantity;
+    void addSugarAndStick(int quantity) {
+        numberOfSugar = Integer.toString(quantity);
+        stick = ONE_STICK;
     }
 }
--- coffeeMachine/src/test/java/DrinkOrderTest.java
+++ coffeeMachine/src/test/java/DrinkOrderTest.java
@@ -4,11 +4,11 @@ import static org.assertj.core.api.Assertions.*;
 
 public class DrinkOrderTest {
 
-    private static final String DRINK_MAKER_MAKES_1_COFFEE = "Drink maker makes 1 coffee with no sugar and therefore no stick";
-    private static final String DRINK_MAKER_MAKES_1_TEA = "Drink maker makes 1 tea with no sugar and therefore no stick";
-    private static final String DRINK_MAKER_MAKES_1_CHOCOLATE = "Drink maker makes 1 chocolate with no sugar and therefore no stick";
-    private static final String DRINK_MAKER_MAKES_1_COFFEE_WITH_1_SUGAR = "Drink maker makes 1 coffee with 1 sugar and a stick";
-    private static final String DRINK_MAKER_MAKES_1_COFFEE_WITH_2_SUGAR = "Drink maker makes 1 coffee with 2 sugars and a stick";
+    private static final String DRINK_MAKER_MAKES_1_COFFEE = "Drink maker makes 1 coffee with no sugar(s) and therefore no stick";
+    private static final String DRINK_MAKER_MAKES_1_TEA = "Drink maker makes 1 tea with no sugar(s) and therefore no stick";
+    private static final String DRINK_MAKER_MAKES_1_CHOCOLATE = "Drink maker makes 1 chocolate with no sugar(s) and therefore no stick";
+    private static final String DRINK_MAKER_MAKES_1_COFFEE_WITH_1_SUGAR = "Drink maker makes 1 coffee with 1 sugar(s) and a stick";
+    private static final String DRINK_MAKER_MAKES_1_COFFEE_WITH_2_SUGAR = "Drink maker makes 1 coffee with 2 sugar(s) and a stick";
 
     @Test
     public void should_send_one_coffee_order_when_customer_order_coffe() {
@@ -29,16 +29,16 @@ public class DrinkOrderTest {
     }
 
     @Test
-    public void should_send_one_coffee_with_one_sugar_order_to_coffee_maker() throws Exception {
+    public void should_send_one_coffee_with_one_sugar_order_to_coffee_maker() {
         DrinkOrder drinkOrder = new DrinkOrder(DrinkType.COFFEE);
-        drinkOrder.addSugar(1);
+        drinkOrder.addSugarAndStick(1);
         assertThat(drinkOrder.createMessageToDrinkMaker()).isEqualTo(DRINK_MAKER_MAKES_1_COFFEE_WITH_1_SUGAR);
     }
 
     @Test
-    public void should_send_one_coffee_with_two_sugar_order_to_coffee_maker() throws Exception {
+    public void should_send_one_coffee_with_two_sugar_order_to_coffee_maker() {
         DrinkOrder drinkOrder = new DrinkOrder(DrinkType.COFFEE);
-        drinkOrder.addSugar(2);
+        drinkOrder.addSugarAndStick(2);
         assertThat(drinkOrder.createMessageToDrinkMaker()).isEqualTo(DRINK_MAKER_MAKES_1_COFFEE_WITH_2_SUGAR);
     }
 }
